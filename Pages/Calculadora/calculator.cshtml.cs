a55ae595f679b35f5289edf0930860724b070422
ErickRossine2022 <odsblankil@gmail.com>
2025-05-17 04:13:03 -0300

    Adicionar arquivos de projeto.

---
+++ Pages/Calculadora/calculator.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Exercicio_HTML___C_.Pages.Calculadora
+{
+    public class CalculatorModel : PageModel
+    {
+        // seu código aqui
+    }
+
+
+}
